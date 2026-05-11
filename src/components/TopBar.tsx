0bfec6ecd914f882ca33b937702dc2aeda5c4e7d
stocktechinvestments <stocktechinvestments@gmail.com>
2026-05-11 17:43:00 +0530

    Update TopBar.tsx

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,7 +5,7 @@ export default function TopBar() {
         <div className='max-[900px]:hidden w-full bg-new-green-300 px-10 py-2 '>
             <div className='w-full flex items-center justify-between'>
                 <a href='https://maps.app.goo.gl/wp2kRxBidJVqACDT7' className='flex gap-2 items-center text-new-dark-300'>
-                    <img src='/assets/logo/location.svg' className='h-[20px]' /> Gangapuri Colony, Mahmoorganj, Varanasi
+                    <img src='/assets/logo/location.svg' className='h-[20px]' /> D.64/42 E-2, Madhopur, Sigra, Varanasi,
                 </a>
                 <a href='mailto:stocktechinvestments@gmail.com' className='flex gap-2 items-center text-new-dark-300'>
                     <img src='/assets/logo/email-2.svg' className='h-[20px]' /> stocktechinvestments@gmail.com
