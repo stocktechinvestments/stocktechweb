c3763a635331fa9204b329902bbe1de932c341c2
stocktechinvestments <stocktechinvestments@gmail.com>
2026-05-11 19:14:50 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,7 +17,7 @@ const ContactMe = () => {
         <div className='w-[400px] bg-new-green-300 rounded-2xl p-5 h-full '>
           <div className='w-full h-full'>
             <div className='w-full mb-5'>
-              <iframe src='https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d536.156808030415!2d82.98500004241434!3d25.3102945564428!3m2!1i1024!2i768!4f13.1!2m1!1sD%2064%2042%20E%202%20Madhopur%20Sigra%20Varanasi!5e0!3m2!1sen!2sin!4v1778501843583!5m2!1sen!2sin' width='600' height='450' style={{ border: 0 }} allowfullscreen=  loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>
+              <iframe src='https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d536.156808030415!2d82.98500004241434!3d25.3102945564428!3m2!1i1024!2i768!4f13.1!2m1!1sD%2064%2042%20E%202%20Madhopur%20Sigra%20Varanasi!5e0!3m2!1sen!2sin!4v1778501843583!5m2!1sen!2sin' width='600' height='450' style={{ border: 0 }} allowfullscreen loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>
             </div>
             <div className='w-full'>
               <a href='https://maps.app.goo.gl/mLtCu3wyj1Ny5rLE9' target='_blank' className='text-xl text-new-blue-300 font-semibold'>
